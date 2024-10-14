1dd4a961acadf6f7c60eb070beb153d4c739d6f0
Samuel Kwibe <121809529+Samkwibe@users.noreply.github.com>
2024-10-14 13:14:44 -0400

    d

---
+++ CKM-Quick-Food-Finder/app/src/main/java/sam/rayl/ckm_quick_food_finder_app/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package sam.rayl.ckm_quick_food_finder_app.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
